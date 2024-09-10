6f74080bb01d16297a6f5c56973d9a9aff84220f
nestor2312 <nestorcanal01@gmail.com>
2024-09-10 12:07:57 -0500

    serve

--- src/ConfigAPI.js
+++ src/ConfigAPI.js
@@ -3,5 +3,5 @@
 // export const API_ENDPOINT = "http://localhost:8000/api";
 // export const IMAGES_URL = "http://localhost:8000/storage/uploads";
 
-export const API_ENDPOINT = "https://full-shirts-draw.loca.lt/api";
-export const IMAGES_URL = "https://full-shirts-draw.loca.lt/storage/uploads";
+export const API_ENDPOINT = "https://wild-plants-drum.loca.lt/api";
+export const IMAGES_URL = "https://wild-plants-drum.loca.lt/storage/uploads";
