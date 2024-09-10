2081f2927fda61af9addbe41c5bfd89b516a9389
nestor2312 <nestorcanal01@gmail.com>
2024-09-10 14:27:56 -0500

    serve2

--- src/ConfigAPI.js
+++ src/ConfigAPI.js
@@ -3,5 +3,9 @@
 // export const API_ENDPOINT = "http://localhost:8000/api";
 // export const IMAGES_URL = "http://localhost:8000/storage/uploads";
 
-export const API_ENDPOINT = "https://wild-plants-drum.loca.lt/api";
-export const IMAGES_URL = "https://wild-plants-drum.loca.lt/storage/uploads";
+// export const API_ENDPOINT = "https://wild-plants-drum.loca.lt/api";
+// export const IMAGES_URL = "https://wild-plants-drum.loca.lt/storage/uploads";
+
+
+export const API_ENDPOINT = "https://73cf-2800-e2-8b80-18-2d48-f39a-dc79-e141.ngrok-free.app/api";
+export const IMAGES_URL = "https://73cf-2800-e2-8b80-18-2d48-f39a-dc79-e141.ngrok-free.app/storage/uploads";
