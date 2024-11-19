e2117031f6bde3a8451c4318762f76356b6084df
nestor2312 <nestorcanal01@gmail.com>
2024-11-19 14:43:32 -0500

    responsive v1

--- src/ConfigAPI.js
+++ src/ConfigAPI.js
@@ -3,8 +3,8 @@
 // export const API_ENDPOINT = "http://localhost:8000/api/";
 // export const IMAGES_URL = "http://localhost:8000/storage/uploads";
 
-export const API_ENDPOINT = " https://witty-regions-read.loca.lt/api/";
-export const IMAGES_URL = " https://witty-regions-read.loca.lt/storage/uploads";
+export const API_ENDPOINT = "https://early-pets-joke.loca.lt/api/";
+export const IMAGES_URL = "https://early-pets-joke.loca.lt/storage/uploads";
 
 
 // export const API_ENDPOINT = "https://73cf-2800-e2-8b80-18-2d48-f39a-dc79-e141.ngrok-free.app/api";
