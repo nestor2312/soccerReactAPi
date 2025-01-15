c186e72928eae6f1b7759b696c04ccaf36e80292
nestor2312 <nestorcanal01@gmail.com>
2025-01-15 13:48:07 -0500

    v1

--- src/ConfigAPI.js
+++ src/ConfigAPI.js
@@ -3,8 +3,8 @@
 // export const API_ENDPOINT = "http://localhost:8000/api/";
 // export const IMAGES_URL = "http://localhost:8000/storage/uploads";
 
-export const API_ENDPOINT = "http://3.144.111.24/api/";
-export const IMAGES_URL = "http://3.144.111.24/storage/uploads";
+export const API_ENDPOINT = "http://ec2-3-145-89-131.us-east-2.compute.amazonaws.com/api/";
+export const IMAGES_URL = "http://ec2-3-145-89-131.us-east-2.compute.amazonaws.com/storage/uploads";
 
 
 // export const API_ENDPOINT = "https://major-experts-sin.loca.lt/api/";
