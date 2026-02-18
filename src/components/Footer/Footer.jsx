f58ce73836fc1546d62c2c09c98592ccaf5cc0bb
nestor2312 <nestorcanal01@gmail.com>
2026-02-18 14:13:06 -0500

    logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,22 @@
 import "./index.css"
+
+import Logo from "../../assets/Logo_liga.png"
 const Footer =()=>{
     return <>
     <footer className="mt-2">
+        <div>
+             <img
+                           src={`${Logo}`}
+                          
+                           className="logo_footer"
+                           alt={Logo}
+                           onError={(e) => {
+                             e.target.onerror = null;
+                            
+                             e.target.classList.add("error-logo");
+                           }}
+                         />
+        </div>
         <p className="text_footer" >&copy; 2025 Fubol Todos los derechos reservados <br/>
         Desarrollado y diseñado por <strong>Nestor Canal</strong> </p>
         
